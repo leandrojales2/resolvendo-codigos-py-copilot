219a9b33773753391d686664b21bee8eca34ebcb
Leandro Jales <140167270+leandrojales2@users.noreply.github.com>
2025-01-23 18:26:58 +0000

    aplicação dos exercicios solicitados

---
+++ resolucoes_code/par_impar.py
@@ -0,0 +1,12 @@
+#Como entrada, receba um número inteiro e verifique se ele é par ou ímpar.
+
+num = int(input("Digite um número inteiro: "))
+
+if num % 2 == 0:
+    print("O número digitado é par.")
+else:
+    print("O número digitado é ímpar.")
+
+
+
+
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,9 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Agora vamos solicitar uma string e um número inteiro como entrada.
+
+string_input = input("Digite uma string: ")
+integer_input = int(input("Digite um número inteiro: "))
+
+# Vamos repetir a string digitada a quantidade de vezes que foi digitado o número inteiro.
+print((string_input + "\n" ) * integer_input)
+
+
